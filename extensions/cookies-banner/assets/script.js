34896b35155c128479e4f3a3aca7e3a832e7c024
Andriy Hladkevych <andriyglad@gmail.com>
2022-12-26 23:58:22 +0200

    Added customise for cookies banner

--- extensions/cookies-banner/assets/script.js
+++ extensions/cookies-banner/assets/script.js
@@ -1,1 +1,4 @@
-console.log(document);
+const cookiesBanner = document.querySelector('.cookies-banner');
+const cookiesMessage = cookiesBanner.querySelector('.cookies-banner__message');
+
+console.log(cookiesMessage);
