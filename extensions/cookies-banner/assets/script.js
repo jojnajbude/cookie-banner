6304298901adf4a01a098b3693d2f26d1e9e63e1
Andriy Hladkevych <andriyglad@gmail.com>
2022-12-27 00:38:23 +0200

    Added links for message

--- extensions/cookies-banner/assets/script.js
+++ extensions/cookies-banner/assets/script.js
@@ -1,4 +1,29 @@
 const cookiesBanner = document.querySelector('.cookies-banner');
-const cookiesMessage = cookiesBanner.querySelector('.cookies-banner__message');
 
-console.log(cookiesMessage);
+const setLinksInMessage = () => {
+  const cookiesMessage = cookiesBanner.querySelector('.cookies-banner__message');
+  const linksContainer = JSON.parse(
+    cookiesBanner
+    .querySelector('.cookies-banner__links-container')
+    .innerHTML
+  );
+
+  for (const link in linksContainer) {
+    cookiesMessage.innerHTML = cookiesMessage.innerHTML
+      .replace(
+        link,
+        `<a
+          href=${linksContainer[link]}
+          class="cookies-banner__link"
+        >
+          ${link}
+        </a>`);
+  }
+};
+
+const handlerCloseBanner = () => {
+  console.log('here');
+  cookiesBanner.style.display = 'none';
+};
+
+setLinksInMessage();
