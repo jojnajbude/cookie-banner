109c351a734a2bd8a993d58fee28b8056add0162
Andriy Hladkevych <andriyglad@gmail.com>
2022-12-26 23:06:35 +0200

    Implemented styles for cookies banner

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,18 +1,7 @@
 import {
-  Card,
   Page,
-  Layout,
-  TextContainer,
-  Image,
-  Stack,
-  Link,
-  Heading,
 } from "@shopify/polaris";
 import { TitleBar } from "@shopify/app-bridge-react";
-
-import { trophyImage } from "../assets";
-
-import { ProductsCard } from "../components";
 
 export default function HomePage() {
   return (
