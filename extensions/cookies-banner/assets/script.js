546c71140b291b29a6f452bc0a4bd2d6fedd9874
Andriy Hladkevych <andriyglad@gmail.com>
2022-12-27 13:06:35 +0200

    Removed unnecessary settings for links, apply the primary face for the app

--- extensions/cookies-banner/assets/script.js
+++ extensions/cookies-banner/assets/script.js
@@ -1,29 +1,8 @@
 const cookiesBanner = document.querySelector('.cookies-banner');
 
-const setLinksInMessage = () => {
-  const cookiesMessage = cookiesBanner.querySelector('.cookies-banner__message');
-  const linksContainer = JSON.parse(
-    cookiesBanner
-    .querySelector('.cookies-banner__links-container')
-    .innerHTML
-  );
-
-  for (const link in linksContainer) {
-    cookiesMessage.innerHTML = cookiesMessage.innerHTML
-      .replace(
-        link,
-        `<a
-          href=${linksContainer[link]}
-          class="cookies-banner__link"
-        >
-          ${link}
-        </a>`);
-  }
-};
-
 const handlerCloseBanner = () => {
-  console.log('here');
   cookiesBanner.style.display = 'none';
 };
 
-setLinksInMessage();
+const closeButton = cookiesBanner.querySelector('.cookies-banner__close-button');
+closeButton.onclick = handlerCloseBanner;
--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,18 +1,46 @@
 import {
+  Icon,
+  Layout,
   Page,
+  TextContainer,
 } from "@shopify/polaris";
+
+import {
+  ToolsMajor
+} from '@shopify/polaris-icons';
+
 import { TitleBar } from "@shopify/app-bridge-react";
 
 export default function HomePage() {
+
+
   return (
     <Page>
       <TitleBar
         title="Cookies Banner"
         primaryAction={{
           content: "Click",
-          onAction: () => {alert('Clicked')}, 
+          onAction: () => {alert('Why are You clicked me?')}, 
         }}
       />
+
+      <Layout>
+        <Layout.Section>
+          <TextContainer>
+            <div style={{ display: 'flex', gap: 10 }}>
+              <p>
+                Cookies Banner Front App is under development
+              </p>
+              <span>
+                <Icon
+                  source={ToolsMajor}
+                  color="base"
+                />
+              </span>
+            </div>
+          </TextContainer>
+        </Layout.Section>
+      </Layout>
     </Page>
   );
 }
